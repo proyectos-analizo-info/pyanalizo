43bef8ce0464de5b9f74209c74664605802c2e4a
Jorge Isnardo Altamirano <jisnardo88@gmail.com>
2015-06-19 18:36:02 +0200

    updated pretty-json.py

--- src/pretty-json.py
+++ src/pretty-json.py
@@ -19,10 +19,13 @@ import sys
 sys.path.append('./shared')
 import functions as f
 
-if __name__ == "__main__":
+def main():
     args = sys.argv[1:] # 0 - script name
     if len(args) >= 1:
         for item in args:
             f.write_json_file(item.split('.')[0] + '-pretty.json', f.parse_json_file(item), {'indent': 4, 'sort_keys': True})
         
     
+
+if __name__ == '__main__':
+    main()
