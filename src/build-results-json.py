fa31833d77bf0209d82ae48e6564630c6b3ee205
Jorge Isnardo Altamirano <jisnardo88@gmail.com>
2015-06-19 18:32:50 +0200

    mv to build-results-json.py

--- src/build-results-json.py
+++ src/build-results-json.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+'''
+
 import sys
 sys.path.append('./shared')
 import functions as f
@@ -55,4 +69,4 @@ if __name__ == '__main__':
         
         d_results[idx]['answers'][idu] = d_user
     
-    f.write_json_file('results_v2.json', d_results)
+    f.write_json_file('results.json', d_results)
